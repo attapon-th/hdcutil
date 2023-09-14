9151e24749abd29d2a278d37586ad61614d05b61
Attapon Thanawong <attapon_srem@hotmail.com>
2023-09-14 16:36:03 +0700

    0.1.8

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def list_files(directory):
 
 setup(
     name="hdcutil",
-    version="0.1.7",
+    version="0.1.8",
     author="attapon.th",
     maintainer="attapon.th",
     maintainer_email="attapon.4work@gmial.com",
