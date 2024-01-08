82ab1a7beda8afb4315607d5227011b5c9f75f9d
ATTAPON THANAWANG <Attapon_srem@hotmail.com>
2024-01-08 12:32:26 +0700

    0.4.2

--- hdcutil/__init__.py
+++ hdcutil/__init__.py
@@ -1,5 +1,5 @@
 from .colookup import CoLookup
-from hdcfile import HDCFiles, init_pandas_options, ALL_HOSPCODE
+from .hdcfile import HDCFiles, init_pandas_options, ALL_HOSPCODE
 
 
 __all__ = ["init_pandas_options", "CoLookup", "HDCFiles", "ALL_HOSPCODE"]
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def list_files(directory: str) -> list[str]:
 
 setup(
     name="hdcutil",
-    version="0.4.1",
+    version="0.4.2",
     author="attapon.th",
     maintainer="attapon.th",
     maintainer_email="attapon.4work@gmial.com",
